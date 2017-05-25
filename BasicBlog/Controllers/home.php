143e258a43d32baaa11ac704495d0c24ddede7c0
ozanbote <ozanbote@windowslive.com>
2017-05-25 23:37:37 +0300

    Deleted home.php
    Deleted home.php

--- BasicBlog/Controllers/home.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Project\Controllers;
-
-class Home extends Controller
-{
-    public function main(String $params = NULL)
-    {
-        // Simplicity is our choice, how about yours ?
-    }
-}
